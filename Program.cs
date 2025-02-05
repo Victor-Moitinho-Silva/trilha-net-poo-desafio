2a805994fa2ea172126f53daa32fba7de64dcb62
Victor Moitinho <victormoitinho10@hotmail.com>
2025-02-05 19:57:07 -0300

    Desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia("98155078", "Nokia28", "eii9899o", 4);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+
+Console.WriteLine("Smartphone Iphone: ");
+Smartphone iphone = new Iphone("98155078", "Iphone 16", "eii9899o", 10);
+iphone.Ligar();
+iphone.InstalarAplicativo("Facebook");
